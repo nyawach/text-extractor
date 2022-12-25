6010201167769daa19cb06bb4444b88c48c5a8a5
nyawach <yuunyawach@gmail.com>
2022-12-25 18:21:15 +0900

    move to .tsx file

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,22 +1,89 @@
-import { useEffect, FC } from 'react'
+import { FC } from 'react'
+
+type CreateRectanglesMessageType = {
+    type: 'create-rectangles'
+    count: number
+}
+
+type CancelMessageType = {
+    type: 'cancel'
+}
+
+type MessageTypes = CreateRectanglesMessageType | CancelMessageType
+
+type PostMessagePayload = {
+    pluginMessage: MessageTypes
+}
+
+const NODE_TYPES: NodeType[] = [
+    'BOOLEAN_OPERATION',
+    'CODE_BLOCK',
+    'COMPONENT',
+    'COMPONENT_SET',
+    'CONNECTOR',
+    'DOCUMENT',
+    'ELLIPSE',
+    'EMBED',
+    'FRAME',
+    'GROUP',
+    'HIGHLIGHT',
+    'INSTANCE',
+    'LINE',
+    'LINK_UNFURL',
+    'MEDIA',
+    'PAGE',
+    'POLYGON',
+    'RECTANGLE',
+    'SECTION',
+    'SHAPE_WITH_TEXT',
+    'SLICE',
+    'STAMP',
+    'STAR',
+    'STICKY',
+    'TEXT',
+    'VECTOR',
+    'WASHI_TAPE',
+    'WIDGET',
+]
+
+const TARGET_ORIGIN = '*'
 
 const App: FC = () => {
-    useEffect(() => {
-        const createButton = document.getElementById('create')
-    
-        createButton?.addEventListener('click', () => {
-            const textbox = document.getElementById('count');
-            const count = parseInt((textbox as HTMLInputElement)?.value ?? '0', 10);
-            parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
-        })
-        
-        const cancelButton = document.getElementById('cancel')
-        
-        cancelButton?.addEventListener('click', () => {
-            parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
-        })
-    }, [])
-    return (<></>)
+    const handleClickCreate = () => {
+        const textbox = document.getElementById('count');
+        const count = parseInt((textbox as HTMLInputElement)?.value ?? '0', 10);
+        const payload: PostMessagePayload = {
+            pluginMessage: {
+                type: 'create-rectangles',
+                count,
+            },
+        }
+        parent.postMessage(payload, TARGET_ORIGIN)
+    }
+    const handleClickCancel = () => {
+        const payload: PostMessagePayload = {
+            pluginMessage: { type: 'cancel' }
+        }
+        parent.postMessage(payload, TARGET_ORIGIN)
+    }
+    return (
+        <>
+            <h2>Text Extractor</h2>
+            <button id="create" onClick={handleClickCreate}>Create</button>
+            <button id="cancel" onClick={handleClickCancel}>Cancel</button>
+            <ul>
+                {NODE_TYPES.map(nodeType => (
+                    <li>
+                        <label>
+                            <input type="checkbox" value={nodeType} />
+                            {nodeType}
+                        </label>
+                    </li>
+                ))}
+            </ul>
+
+        </>
+    )
 }
 
 export default App
